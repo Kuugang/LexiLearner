b26c74dfaf0bee07fbeb8f9dc84c97927a118d6c
Kuugang <jakebajo11@gmail.com>
2025-05-21 09:07:42 +0800

    saved api url

--- lexilearner/utils/constants.js
+++ lexilearner/utils/constants.js
@@ -5,5 +5,7 @@
 
 // export const API_URL = "http://127.0.0.1:5144/api";
 // export const API_URL = "http://192.168.254.103:5144/api";
-export const API_URL = "http://192.168.254.103:5144/api";
-// export const API_URL = "http://192.168.1.28:5144/api";
+//export const API_URL = "http://192.168.254.103:5144/api";
+
+//JAKE
+export const API_URL = "http://192.168.1.28:5144/api";
