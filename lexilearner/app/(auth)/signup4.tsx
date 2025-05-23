e6d9107ad472d354cbd86ea4560c0687127fc3ec
Kuugang <jakebajo11@gmail.com>
2025-05-23 15:44:07 +0800

    refactor auth pages

--- lexilearner/app/(auth)/signup4.tsx
+++ lexilearner/app/(auth)/signup4.tsx
@@ -5,7 +5,21 @@ import { useGlobalStore } from "@/stores/globalStore";
 import { refreshAccessToken } from "@/services/AuthService";
 import Toast from "react-native-toast-message";
 
-import SignUp4 from "@/components/Auth/SignUp4";
+import { Platform } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+
+import { View, Image } from "react-native";
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
 
 export default function Step4() {
   const updateProfile = useUserStore((state) => state.updateProfile);
@@ -13,6 +27,17 @@ export default function Step4() {
 
   const [age, setAge] = useState<string | null>(null);
   const [ageInvalid, setAgeInvalid] = useState(false);
+
+  const insets = useSafeAreaInsets();
+  const contentInsets = {
+    top: insets.top,
+    bottom: Platform.select({
+      android: insets.bottom,
+      default: insets.bottom,
+    }),
+    left: 12,
+    right: 12,
+  };
 
   const handleStep = async () => {
     try {
@@ -41,6 +66,65 @@ export default function Step4() {
   };
 
   return (
-    <SignUp4 ageInvalid={ageInvalid} setAge={setAge} handleStep={handleStep} />
+    <ScrollView className="bg-yellowOrange">
+      <View className="flex-1 gap-14 p-8 h-full justify-around items-center">
+        <View className="flex-1 gap-6 p-8 h-full justify-around items-center">
+          <Image
+            source={require("@/assets/images/role-pupil.png")}
+            style={{ width: 160, height: 160 }}
+            resizeMode="contain"
+            alt=""
+          />
+          <Text className="text-typography-black font-bold text-3xl">
+            Almost There!
+          </Text>
+        </View>
+
+        <Select
+          onValueChange={(e) => {
+            setAge(e?.value as string);
+          }}
+        >
+          <SelectTrigger className="w-[250px]">
+            <SelectValue
+              className="text-foreground text-sm native:text-lg"
+              placeholder="Enter your age"
+            />
+          </SelectTrigger>
+
+          <SelectContent insets={contentInsets} side="bottom">
+            <ScrollView className="w-[245px] max-h-64">
+              <SelectLabel>Age</SelectLabel>
+              {[
+                6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+              ].map((value) => (
+                <SelectItem
+                  key={value}
+                  label={String(value)}
+                  value={String(value)}
+                >
+                  {value}
+                </SelectItem>
+              ))}
+            </ScrollView>
+          </SelectContent>
+        </Select>
+
+        {ageInvalid && (
+          <Text className="text-lg text-destructive text-center">
+            Please enter your age.
+          </Text>
+        )}
+
+        <Button
+          className="bg-orange rounded-lg mt-6"
+          onPress={() => {
+            handleStep();
+          }}
+        >
+          <Text className="text-white text-2xl font-bold">Finish</Text>
+        </Button>
+      </View>
+    </ScrollView>
   );
 }
