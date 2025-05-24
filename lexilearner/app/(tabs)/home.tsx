36df2b7d6262b269405568b43dcb45d1d45f7584
Kuugang <jakebajo11@gmail.com>
2025-05-24 13:12:38 +0800

    home page recommendations

--- lexilearner/app/(tabs)/home.tsx
+++ lexilearner/app/(tabs)/home.tsx
@@ -1,5 +1,8 @@
 import { router } from "expo-router";
-import { useStories } from "@/services/ReadingMaterialService";
+import {
+  useRecommendedStories,
+  useStories,
+} from "@/services/ReadingMaterialService";
 import { memo, useEffect, useState } from "react";
 import ReadingContent from "@/components/ReadingContent";
 import LoginStreak from "@/components/LoginStreak";
@@ -17,6 +20,7 @@ import { HeaderSearchBar } from "@/components/HeaderSearchBar";
 
 function HomeScreen() {
   const { data: stories, isLoading: isStoriesLoading } = useStories();
+  const { data: recommendations } = useRecommendedStories();
   const [showStreak, setShowStreakModal] = useState(false);
   const user = useUserStore((state) => state.user);
   const lastLoginStreak = useUserStore((state) => state.lastLoginStreak);
@@ -44,7 +48,7 @@ function HomeScreen() {
   const activeWeekdays = [true, true, true, false, false, false, false];
 
   const [screenWidth, setScreenWidth] = useState(
-    Dimensions.get("window").width
+    Dimensions.get("window").width,
   );
 
   useEffect(() => {
@@ -52,7 +56,7 @@ function HomeScreen() {
       "change",
       ({ window }) => {
         setScreenWidth(window.width);
-      }
+      },
     );
 
     return () => {
@@ -107,37 +111,25 @@ function HomeScreen() {
 
       <View className="flex-1  w-full h-60 p-4">
         <Text className="text-2xl font-bold">Recommended</Text>
-
-        <ReadingContent
-          type={"Recommended"}
-          id={"123"}
-          content={`
-    This edition is part of the Beginner Books collection, which aims to encourage early readers with its simple language and visual storytelling. It’s a perfect book for both independent reading and family read-aloud sessions!
-
-
-    "Cat in the Hat" is a delightful and whimsical story by Dr. Seuss, packed with fun and chaos. The plot revolves around a young brother and sister who are stuck indoors on a rainy day. Their boredom is quickly turned upside down when the mischievous Cat in the Hat shows up, bringing along his troublesome friends, Thing 1 and Thing 2. Together, they cause mayhem and mess, but they also help turn the day into an unforgettable adventure. 
-
-    The Cat’s antics and the antics of his companions are a source of laughter and imagination, all while teaching important lessons about responsibility and the consequences of causing trouble. This story is known for its simple rhymes and repetitive words, making it perfect for young readers to follow along with and build their reading skills. Whether it’s parents, teachers, or kids, "Cat in the Hat" has earned a lasting spot in the hearts of many for its playful nature and engaging characters.
-
-    Dr. Seuss’s trademark use of vibrant illustrations and clever wordplay makes the story even more fun. The characters like Thing 1 and Thing 2, and the talking fish, add to the charm and humor. The book's smaller format and easy-to-read design make it great for kids aged 3-7 to practice reading on their own. 
-
-    This edition is part of the Beginner Books collection, which aims to encourage early readers with its simple language and visual storytelling. It’s a perfect book for both independent reading and family read-aloud sessions!
-    This edition is part of the Beginner Books collection, which aims to encourage early readers with its simple language and visual storytelling. It’s a perfect book for both independent reading and family read-aloud sessions!
-    This edition is part of the Beginner Books collection, which aims to encourage early readers with its simple language and visual storytelling. It’s a perfect book for both independent reading and family read-aloud sessions!
-    This edition is part of the Beginner Books collection, which aims to encourage early readers with its simple language and visual storytelling. It’s a perfect book for both independent reading and family read-aloud sessions!
-  `}
-          title={"Cat In The Hat"}
-          author={"Dr. Seuss"}
-          description="Have a ball with Dr. Seuss and the Cat in the Hat in this classic picture book...but don't forget to clean up your mess!
-Then he said That is that.
-And then he was gone
-With a tip of his hat.
-A dreary day turns into a wild romp when this beloved story introduces readers to the Cat in the Hat and his troublemaking friends, Thing 1 and Thing 2 – And don't forget Fish! A favorite among kids, parents and teachers, this story uses simple words and basic rhyme to encourage and delight beginning readers.
-Originally created by Dr. Seuss himself, Beginner Books are fun, funny, and easy to read. These unjacketed hardcover early readers encourage children to read all on their own, using simple words and illustrations. Smaller than the classic large format Seuss picture books like The Lorax and Oh, The Places You'll Go!, these portable packages are perfect for practicing readers ages 3-7, and lucky parents too!"
-          cover="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1468890477i/233093.jpg"
-          genres={["Fiction"]}
-          difficulty={10}
-        />
+        <ScrollView horizontal={true}>
+          {recommendations &&
+            recommendations.length > 0 &&
+            recommendations.map((r) => (
+              <View className="w-[90vw]">
+                <ReadingContent
+                  type={"Recommended"}
+                  id={r.id}
+                  content={r.content}
+                  title={r.title}
+                  author={r.author}
+                  description={r.description}
+                  cover={r.cover}
+                  genres={r.genres}
+                  difficulty={r.difficulty}
+                />
+              </View>
+            ))}
+        </ScrollView>
       </View>
 
       <View className="flex-1 gap-4 w-full p-8">
