43b74210e53c938b260e3dff2ebb1fa3d6500f89
chrlskrt <charlene.repuesto@cit.edu>
2025-05-29 11:32:50 +0800

    Comment out unwanted code

--- lexilearner/utils/constants.js
+++ lexilearner/utils/constants.js
@@ -12,10 +12,13 @@
 // export const API_URL = "http://192.168.68.102:5144/api";
 
 //JAKE
-export const API_URL = "http://192.168.254.104:5144/api";
+// export const API_URL = "http://192.168.254.104:5144/api";
 
 // DEO
 //export const API_URL = "http://192.168.1.3:5144/api";
 
 // CHARLENE
 // export const API_URL = "http://192.168.100.35:5144/api";
+
+export const API_URL = "http://192.168.113.170:5144/api";
+
